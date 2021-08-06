d4a047871d9797224e2c95344242046859fc1780
gianmarco lunardi <gianmarcofi@gmail.com>
2021-08-06 10:28:21 +0200

    Table Guest Added Aggiungere i file di progetto.

---
+++ Agriturismo/Models/Guest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Agriturismo.Models
+{
+    public class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(15)]
+        public String Name { get; set; }
+        [MaxLength(15)]
+        public String Surnamer { get; set; }
+        [MaxLength(50)]
+        public String Address { get; set; }
+        [MaxLength(15)]
+        public String City { get; set; }
+        [MaxLength(15)]
+        public String Zip { get; set; }
+        [MaxLength(15)]
+        public String Country { get; set; }
+        [MaxLength(15)]
+
+        public String Phone { get; set; }
+
+
+    }
+}
