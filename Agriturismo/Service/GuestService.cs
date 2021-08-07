04e0b3190f88c9a1b3cdf384e623eeee6f124eb4
gianmarco lunardi <gianmarcofi@gmail.com>
2021-08-07 20:28:47 +0200

    First step of  Service

---
+++ Agriturismo/Service/GuestService.cs
@@ -0,0 +1,42 @@
+﻿using Agriturismo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agriturismo.Service
+{
+    public class GuestService : IGuests
+    {
+        private readonly IGuests GuestContext;
+        public GuestService(IGuests g)
+        {
+            GuestContext = g;
+        }
+        
+        public Task add(Guest t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task delete(Guest t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guest> display(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Guest>> displayAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guest> Update(Guest t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Agriturismo/Service/Interface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Agriturismo.Models;
+
+namespace Agriturismo.Service
+{
+
+    // Generic Interface
+    public interface Irepo<T, in PKey> where T:class
+    {
+        Task add(T t);
+        Task delete(T t);
+
+
+        Task<T>display(PKey x);
+        Task<IEnumerable<T>> displayAll();
+        Task<T> Update(T t);
+
+    }
+
+
+    // Interfa Guests
+
+    public interface IGuests : Irepo<Guest,int> { }
+
+
+}
